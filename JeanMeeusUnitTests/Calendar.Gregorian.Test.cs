424ff3cc8af57277088e27ecd9e49c7628d82751
Marcus Rugger <marcusrugger@yahoo.com>
2017-09-04 23:01:50 -0400

    Add more unit tests for Julian and Gregorian calendars

--- JeanMeeusUnitTests/Calendar.Gregorian.Test.cs
+++ JeanMeeusUnitTests/Calendar.Gregorian.Test.cs
@@ -14,6 +14,15 @@ namespace JeanMeeusUnitTests
             TestGregorianDate(new GregorianCalendar(2436116.31), 1957, Months.October, 4.81);
             TestGregorianDate(new GregorianCalendar(2418781.5), 1910, Months.April, 20.0);
             TestGregorianDate(new GregorianCalendar(2446470.5), 1986, Months.Febuary, 9.0);
+
+            TestGregorianDate(new GregorianCalendar(2451545.0), 2000, 1, 1.5);
+            TestGregorianDate(new GregorianCalendar(2446822.5), 1987, 1, 27.0);
+            TestGregorianDate(new GregorianCalendar(2446966.0), 1987, 6, 19.5);
+            TestGregorianDate(new GregorianCalendar(2447187.5), 1988, 1, 27.0);
+            TestGregorianDate(new GregorianCalendar(2447332.0), 1988, 6, 19.5);
+            TestGregorianDate(new GregorianCalendar(2415020.5), 1900, 1, 1.0);
+            TestGregorianDate(new GregorianCalendar(2305447.5), 1600, 1, 1.0);
+            TestGregorianDate(new GregorianCalendar(2305812.5), 1600, 12, 31.0);
         }
 
         private void TestGregorianDate(Calendar calendar, int expectedYear, int expectedMonth, double expectedDay)
--- JeanMeeusUnitTests/Calendar.Julian.Test.cs
+++ JeanMeeusUnitTests/Calendar.Julian.Test.cs
@@ -12,6 +12,12 @@ namespace JeanMeeusUnitTests
         public void TestJulianToDate()
         {
             TestJulianDate(new JulianCalendar(1842713.0), 333, Months.January, 27.5);
+
+            TestJulianDate(new JulianCalendar(2026871.8), 837, 4, 10.3);
+            TestJulianDate(new JulianCalendar(1356001.0), -1000, 7, 12.5);
+            TestJulianDate(new JulianCalendar(1355866.5), -1000, 2, 29.0);
+            TestJulianDate(new JulianCalendar(1355671.4), -1001, 8, 17.9);
+            TestJulianDate(new JulianCalendar(0.0), -4712, 1, 1.5);
         }
 
         private void TestJulianDate(Calendar calendar, int expectedYear, int expectedMonth, double expectedDay)
