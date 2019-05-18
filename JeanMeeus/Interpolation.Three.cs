4a2442c63da3c36fefd12599ea2e1d4ab2f35b52
Marcus Rugger <marcusrugger@yahoo.com>
2019-05-18 19:14:52 -0400

    Add InterpolationThree
    Class constructor takes 3 pairs of x, y values from which CalculateY can interpolate y given x.

---
+++ JeanMeeus/Interpolation.Three.cs
@@ -0,0 +1,29 @@
+﻿
+namespace JeanMeeus
+{
+    class InterpolationThree
+    {
+        private readonly double x2, y2;
+        private readonly double a, b, c;
+
+        public InterpolationThree(
+            double x1, double y1,
+            double x2, double y2,
+            double x3, double y3)
+        {
+            this.x2 = x2; this.y2 = y2;
+
+            a = y2 - y1;
+            b = y3 - y2;
+            c = b - a;
+        }
+
+        // Astronomical Algorithms by Jean Meeus, Chapter 3, page 25, Formula 3.3
+        public double CalculateY(double x)
+        {
+            double n = x - x2;
+            double y = y2 + (n / 2) * (a + b + n * c);
+            return y;
+        }
+    }
+}
