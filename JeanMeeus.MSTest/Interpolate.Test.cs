e3216033ac022dfdec4b167582bc9313548dd1e7
Marcus Rugger <marcusrugger@yahoo.com>
2019-05-18 23:42:45 -0400

    Add interpolate from five

--- JeanMeeus.MSTest/Interpolate.Test.cs
+++ JeanMeeus.MSTest/Interpolate.Test.cs
@@ -9,8 +9,30 @@ namespace JeanMeeus.MSTest
         [TestMethod]
         public void Example3a()
         {
-            var fn = Interpolate.From(8.0, 0.884226, 0.877366, 0.870531);
+            var fn = Interpolate.FromX(8.0, 0.884226, 0.877366, 0.870531);
             Assert.AreEqual(0.876125, fn(8.18125), 0.000001);
+        }
+
+        // Astronomical Algorithms by Jean Meeus, Chapter 3, page 28, Example 3.e
+        [TestMethod]
+        public void Example3e()
+        {
+            var fn = Interpolate.FromX(
+                28.0,
+                Sexagesimal(0, 54, 36.125),
+                Sexagesimal(0, 54, 24.606),
+                Sexagesimal(0, 54, 15.486),
+                Sexagesimal(0, 54, 8.694),
+                Sexagesimal(0, 54, 4.133));
+
+            double n = Sexagesimal(3, 20, 0) / 12.0;
+            double x = 28.0 + n;
+            Assert.AreEqual(Sexagesimal(0, 54, 13.369), fn(x), 0.0000278);
+        }
+
+        private double Sexagesimal(double h, double m, double s)
+        {
+            return h + m / 60.0 + s / 3600.0;
         }
     }
 }
--- JeanMeeus/Interpolate.cs
+++ JeanMeeus/Interpolate.cs
@@ -5,11 +5,18 @@ namespace JeanMeeus
 
     public class Interpolate
     {
-        public static InterpolateY From(double x2, double y1, double y2, double y3)
+        public static InterpolateY FromX(double x2, double y1, double y2, double y3)
         {
             var fn = new InterpolateThree(x2, y1, y2, y3);
 
-            return (x) => fn.CalculateY(x);
+            return (x) => fn.FromX(x);
+        }
+
+        public static InterpolateY FromX(double x3, double y1, double y2, double y3, double y4, double y5)
+        {
+            var fn = new InterpolateFive(x3, y1, y2, y3, y4, y5);
+
+            return (x) => fn.FromX(x);
         }
     }
 
@@ -30,11 +37,59 @@ namespace JeanMeeus
             c = b - a;
         }
 
-        // Astronomical Algorithms by Jean Meeus, Chapter 3, page 25, Formula 3.3
-        public double CalculateY(double x)
+        public double FromX(double x)
         {
-            double n = x - x2;
+            var n = x - x2;
+            return FromN(n);
+        }
+
+        // Astronomical Algorithms by Jean Meeus, Chapter 3, page 25, Formula 3.3
+        public double FromN(double n)
+        {
             double y = y2 + (n / 2) * (a + b + n * c);
+            return y;
+        }
+    }
+
+    public class InterpolateFive
+    {
+        private readonly double x3, y3;
+        private readonly double a, b, c, d, e, f, g, h, j, k;
+
+        public InterpolateFive(
+            double x3,
+            double y1, double y2, double y3, double y4, double y5)
+        {
+            this.x3 = x3;
+            this.y3 = y3;
+
+            a = y2 - y1;
+            b = y3 - y2;
+            c = y4 - y3;
+            d = y5 - y4;
+            e = b - a;
+            f = c - b;
+            g = d - c;
+            h = f - e;
+            j = g - f;
+            k = j - h;
+        }
+
+        public double FromX(double x)
+        {
+            double n = x - x3;
+            return FromN(n);
+        }
+
+        // Astronomical Algorithms by Jean Meeus, Chapter 3, page 28, Formula 3.8
+        public double FromN(double n)
+        {
+            double y = y3
+                     + (n / 2) * (b + c)
+                     + (n * n / 2) * f
+                     + ((n * (n * n - 1)) / 12) * (h + j)
+                     + ((n * n * (n * n - 1)) / 24) * k;
+
             return y;
         }
     }
