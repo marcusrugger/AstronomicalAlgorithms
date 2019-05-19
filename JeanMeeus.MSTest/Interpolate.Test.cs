3a8d4d668d6dfc16e3ffbac889f6e0ffa1bb6ca6
Marcus Rugger <marcusrugger@yahoo.com>
2019-05-18 20:43:47 -0400

    Minor refactor

--- JeanMeeus.MSTest/Interpolate.Test.cs
+++ JeanMeeus.MSTest/Interpolate.Test.cs
@@ -3,18 +3,14 @@
 namespace JeanMeeus.MSTest
 {
     [TestClass]
-    public class InterpolateThreeTests
+    public class InterpolateTests
     {
         // Astronomical Algorithms by Jean Meeus, Chapter 3, page 25, Example 3.a
         [TestMethod]
         public void Example3a()
         {
-            var fn = new InterpolateThree(
-                7.0, 0.884226,
-                8.0, 0.877366,
-                9.0, 0.870531);
-
-            Assert.AreEqual(0.876125, fn.CalculateY(8.18125), 0.000001);
+            var fn = Interpolate.From(8.0, 0.884226, 0.877366, 0.870531);
+            Assert.AreEqual(0.876125, fn(8.18125), 0.000001);
         }
     }
 }
--- JeanMeeus/Interpolate.cs
+++ JeanMeeus/Interpolate.cs
@@ -1,17 +1,29 @@
 ﻿
 namespace JeanMeeus
 {
+    public delegate double InterpolateY(double x);
+
+    public class Interpolate
+    {
+        public static InterpolateY From(double x2, double y1, double y2, double y3)
+        {
+            var fn = new InterpolateThree(x2, y1, y2, y3);
+
+            return (x) => fn.CalculateY(x);
+        }
+    }
+
     public class InterpolateThree
     {
         private readonly double x2, y2;
         private readonly double a, b, c;
 
         public InterpolateThree(
-            double x1, double y1,
-            double x2, double y2,
-            double x3, double y3)
+            double x2,
+            double y1, double y2, double y3)
         {
-            this.x2 = x2; this.y2 = y2;
+            this.x2 = x2;
+            this.y2 = y2;
 
             a = y2 - y1;
             b = y3 - y2;
