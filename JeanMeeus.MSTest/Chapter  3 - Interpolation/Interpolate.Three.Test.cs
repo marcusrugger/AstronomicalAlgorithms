9729888b728f1c1c0f2f11d940bb133c3500845b
Marcus Rugger <marcusrugger@yahoo.com>
2019-05-19 23:56:16 -0400

    Refactor interpolation

---
+++ JeanMeeus.MSTest/Chapter  3 - Interpolation/Interpolate.Three.Test.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JeanMeeus.MSTest
+{
+    [TestClass]
+    public class InterpolateThreeTests
+    {
+        private double Sexagesimal(double h, double m, double s)
+        {
+            return Convert.Sexagecimal(h, m, s);
+        }
+
+        // Astronomical Algorithms by Jean Meeus, Chapter 3, page 25, Example 3.a
+        [TestMethod]
+        public void Example3a()
+        {
+            var fn = new InterpolateThree(0.884226, 0.877366, 0.870531);
+            var n = Sexagesimal(4, 21, 0) / 24.0;
+            var y = fn.FromN(n);
+            Assert.AreEqual(0.876125, y, 0.000001);
+        }
+
+        // Astronomical Algorithms by Jean Meeus, Chapter 3, page 25, Example 3.b
+        [TestMethod]
+        public void Example3b()
+        {
+            var fn = new InterpolateThree(1.3814294, 1.3812213, 1.3812453);
+
+            Assert.AreEqual(1.3812030, fn.ExtremumY, 0.0000001);
+            Assert.AreEqual(0.39660, fn.ExtremumN, 0.00001);
+        }
+
+        // Astronomical Algorithms by Jean Meeus, Chapter 3, page 26, Example 3.c
+        [TestMethod]
+        public void Example3c()
+        {
+            var fn = new InterpolateThree(
+                -Sexagesimal(0, 28, 13.4),
+                Sexagesimal(0, 6, 46.3),
+                Sexagesimal(0, 38, 23.2));
+
+            Assert.AreEqual(-0.20127, fn.Ybecomes0(0.00001), 0.00001);
+        }
+
+        // Astronomical Algorithms by Jean Meeus, Chapter 3, page 27, Example 3.d
+        [TestMethod]
+        public void Example3d()
+        {
+            var fn = new InterpolateThree(-2, 3, 2);
+
+            Assert.AreEqual(-0.720759220056, fn.CurvedYbecomes0(0.000000000001), 0.000000000001);
+        }
+    }
+}
---
+++ JeanMeeus/Chapter  3 - Interpolation/Interpolate.Five.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace JeanMeeus
+{
+    public class InterpolateFive
+    {
+        private readonly double y3;
+        private readonly double a, b, c, d, e, f, g, h, j, k;
+
+        public InterpolateFive(double y1, double y2, double y3, double y4, double y5)
+        {
+            this.y3 = y3;
+
+            a = y2 - y1;
+            b = y3 - y2;
+            c = y4 - y3;
+            d = y5 - y4;
+            e = b - a;
+            f = c - b;
+            g = d - c;
+            h = f - e;
+            j = g - f;
+            k = j - h;
+        }
+
+        // Astronomical Algorithms by Jean Meeus, Chapter 3, page 28, Formula 3.8
+        public double FromN(double n)
+        {
+            double y = y3
+                     + (n / 2) * (b + c)
+                     + (n * n / 2) * f
+                     + ((n * (n * n - 1)) / 12) * (h + j)
+                     + ((n * n * (n * n - 1)) / 24) * k;
+
+            return y;
+        }
+    }
+}
