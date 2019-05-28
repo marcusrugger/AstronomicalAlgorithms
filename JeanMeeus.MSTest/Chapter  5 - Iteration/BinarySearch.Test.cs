8228c60fc4ce3e768fe26e0280700fe9a6659205
Marcus Rugger <marcusrugger@yahoo.com>
2019-05-28 06:47:27 -0400

    Minor refactor of binary search iterator
    * Rename class to BinarySearch
    * Make member variables readonly
    * Add Sqrt(2) unit test

--- JeanMeeus.MSTest/Chapter  5 - Iteration/BinarySearch.Test.cs
+++ JeanMeeus.MSTest/Chapter  5 - Iteration/BinarySearch.Test.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace JeanMeeus.MSTest
 {
     [TestClass]
-    public class IterationTests
+    public class BinarySearchTests
     {
         const double PRECISION = 0.000000001;
 
@@ -12,7 +12,7 @@ namespace JeanMeeus.MSTest
         [TestMethod]
         public void TestBinarySearchExample5b()
         {
-            var biterator = Iteration.Create(0, 1, PRECISION);
+            var biterator = BinarySearch.Create(0, 1, PRECISION);
             var result = biterator((x) => Math.Pow(x, 5) + 17 * x - 8);
 
             double expected = 0.469249878;
@@ -23,10 +23,20 @@ namespace JeanMeeus.MSTest
         [TestMethod]
         public void TestBinarySearchExample5c()
         {
-            var biterator = Iteration.Create(1, 2, PRECISION);
+            var biterator = BinarySearch.Create(1, 2, PRECISION);
             var result = biterator((x) => Math.Pow(x, 5) + 3 * x - 8);
 
             double expected = 1.321785627;
+            Assert.AreEqual(expected, result, PRECISION);
+        }
+
+        [TestMethod]
+        public void TestBinarySearchSquareRoot2()
+        {
+            var biterator = BinarySearch.Create(1, 2, PRECISION);
+            var result = biterator((x) => x * x - 2);
+
+            double expected = Math.Sqrt(2.0);
             Assert.AreEqual(expected, result, PRECISION);
         }
     }
