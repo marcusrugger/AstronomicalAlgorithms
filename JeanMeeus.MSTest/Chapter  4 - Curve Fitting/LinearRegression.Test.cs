fb7fa6a795a32fe76d9c1feb5b8202aa572442de
Marcus Rugger <marcusrugger@yahoo.com>
2019-05-30 22:13:07 -0400

    Add coefficient of correlation along with unit test

--- JeanMeeus.MSTest/Chapter  4 - Curve Fitting/LinearRegression.Test.cs
+++ JeanMeeus.MSTest/Chapter  4 - Curve Fitting/LinearRegression.Test.cs
@@ -46,5 +46,45 @@ namespace JeanMeeus.MSTest
             Assert.AreEqual(13.67, line.a, 0.01);
             Assert.AreEqual( 7.03, line.b, 0.01);
         }
+
+        // Astronomical Algorithms by Jean Meeus, Chapter 4, page 40, Example 4.b
+        [TestMethod]
+        public void Example4b()
+        {
+            Point[] points =
+            {
+                // Left column
+                new Point { x = 73, y =  90.4 },
+                new Point { x = 38, y = 125.3 },
+                new Point { x = 35, y = 161.8 },
+                new Point { x = 42, y = 143.4 },
+                new Point { x = 78, y =  52.5 },
+                new Point { x = 68, y =  50.8 },
+                new Point { x = 74, y =  71.5 },
+                new Point { x = 42, y = 152.8 },
+                new Point { x = 52, y = 131.3 },
+                new Point { x = 54, y =  98.5 },
+                new Point { x = 39, y = 144.8 },
+
+                // Right column
+                new Point { x = 61, y =  78.1 },
+                new Point { x = 42, y =  89.5 },
+                new Point { x = 49, y =  63.9 },
+                new Point { x = 50, y = 112.1 },
+                new Point { x = 62, y =  82.0 },
+                new Point { x = 44, y = 119.8 },
+                new Point { x = 39, y = 161.2 },
+                new Point { x = 43, y = 208.4 },
+                new Point { x = 54, y = 111.6 },
+                new Point { x = 44, y = 167.1 },
+                new Point { x = 37, y = 162.1 }
+            };
+
+            var line = new LinearRegression(points);
+
+            Assert.AreEqual( -2.49, line.a, 0.01);
+            Assert.AreEqual(244.18, line.b, 0.01);
+            Assert.AreEqual(-0.767, line.r, 0.001);
+        }
     }
 }
--- JeanMeeus/Chapter  4 - Curve Fitting/LinearRegression.cs
+++ JeanMeeus/Chapter  4 - Curve Fitting/LinearRegression.cs
@@ -12,6 +12,7 @@ namespace JeanMeeus
     {
         public readonly double a;
         public readonly double b;
+        public readonly double r;
 
         public static Func<double, double> Create(Point[] points)
         {
@@ -22,7 +23,7 @@ namespace JeanMeeus
         public LinearRegression(Point[] points)
         {
             double N = points.Length;
-            double Ex = 0, Ey = 0, Exy = 0, Ex2 = 0;
+            double Ex = 0, Ey = 0, Exy = 0, Ex2 = 0, Ey2 = 0;
 
             foreach (var p in points)
             {
@@ -30,10 +31,12 @@ namespace JeanMeeus
                 Ey  += p.y;
                 Exy += p.x * p.y;
                 Ex2 += p.x * p.x;
+                Ey2 += p.y * p.y;
             }
 
             a = (N * Exy - Ex * Ey) / (N * Ex2 - Ex * Ex);
             b = (Ey * Ex2 - Ex * Exy) / (N * Ex2 - Ex * Ex);
+            r = (N * Exy - Ex * Ey) / (Math.Sqrt(N * Ex2 - Ex * Ex) * Math.Sqrt(N * Ey2 - Ey * Ey));
         }
 
         public double f(double x)
